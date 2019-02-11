5249ebcec370cd5e50765456509dfb891d082e91
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-11 15:40:11 -0500

    first commit, added MVC directory structure, installed dependencies, established express router

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('port', process.env.PORT || 3001)
+
+app.listen(app.get('port'), () => {
+    console.log(`✅ PORT: ${app.get('port')} 🌟`)
+  })
